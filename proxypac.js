7f7710421860bc68c1ebad64ba5b90d2e1700301
Ferri <32834985+ferrinomas@users.noreply.github.com>
2022-06-09 08:50:31 +0200

    Update proxypac.js

--- proxypac.js
+++ proxypac.js
@@ -130,8 +130,11 @@ function FindProxyForURL(url, host) { // variable strings to return // a Caixa d
     if (shExpMatch(url, "*.forceusercontent.com*")) {
         return returnProxy(url, host, proxy_caixa, "I");
     }
-	if (shExpMatch(url, "*viewnext*") || shExpMatch(url, "*teams.microsoft.com*") || shExpMatch(url, "*statics.teams.cdn.office.net*") || shExpMatch(url, "*.teams.*") || shExpMatch(url, "*.microsoft.*") || shExpMatch(url, "*slack-core.com*") || shExpMatch(url, "*slack-redir.net*") || shExpMatch(url, "msedge.b.tlu.dl.delivery.mp.microsoft.com") || shExpMatch(url, "2.tlu.dl.delivery.mp.microsoft.com")) {
-        return returnProxy(url, host, proxy_no, "a");
+    if (shExpMatch(url, "*viewnext*") || shExpMatch(url, "*teams.microsoft.com*") || shExpMatch(url, "*statics.teams.cdn.office.net*") || shExpMatch(url, "*.teams.*") || shExpMatch(url, "*.microsoft.*") || shExpMatch(url, "*slack-core.com*") || shExpMatch(url, "*slack-redir.net*") || shExpMatch(url, "msedge.b.tlu.dl.delivery.mp.microsoft.com")) {
+        return returnProxy(url, host, proxy_no, "AA");
+    }
+    if (shExpMatch(url, "2.tlu.dl.delivery.mp.microsoft.com")) {
+        return returnProxy(url, host, proxy_no, "AB");
     } // Proxy anything else
     return returnProxy(url, host, proxy_no, "Y");
 }
